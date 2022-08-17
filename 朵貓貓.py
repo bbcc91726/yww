8f465810990cd40f8016a6516a70a4ebe77aa979
bbcc91726 <111448958+bbcc91726@users.noreply.github.com>
2022-08-18 00:38:18 +0800

    Add files via upload

--- 朵貓貓.py
+++ 朵貓貓.py
@@ -7,12 +7,7 @@ bot = commands.Bot(command_prefix = '!')
 async def on_ready():
     print(f'Bot is ready')
 
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(890071214356324424)
-    await channel.send(f'{mamber} join!')
-    
 
 
-bot.run('MTAwOTMzNjM1OTkyNTg1MDIyMw.Gf40yH.D-pSBYo66rhyc5ZMoWR2nnuajFTPDriPsJDBX4')
+bot.run('MTAwOTMzNjM1OTkyNTg1MDIyMw.GeRtnu.xRp5fIhfWcV93Peav-Hf1mRf_-l54b8z9YeZmc')
 
